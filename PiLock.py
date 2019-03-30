bd1ae8a76cfe138fce53d96ae084f2b4e2b66495
smalbadger <smalbadger@email.arizona.edu>
2019-03-30 13:57:28 -0700

    fixed indentation

--- PiLock.py
+++ PiLock.py
@@ -29,62 +29,62 @@ keypad = KeyPad(21,20,16,12,25)
 
 def lock():
     """ sequence of instructions to go into LOCKED mode"""
-	global disp
-	global indicator
-	global state
-	global keypad
-	global passcode
-	global lockMotor
-	
-	passcode = keypad.getGuess()
-	disp.writeMessage("Locked")
-	indicator.showRed()
-	state = LOCKED
-	keypad.reset()
-	lockMotor.lock()
-	
-	
+    global disp
+    global indicator
+    global state
+    global keypad
+    global passcode
+    global lockMotor
+    
+    passcode = keypad.getGuess()
+    disp.writeMessage("Locked")
+    indicator.showRed()
+    state = LOCKED
+    keypad.reset()
+    lockMotor.lock()
+    
+    
 def unlock():
     """ sequence of instructions to go into UNLOCKED mode"""
-	global disp
-	global indicator
-	global state
-	global keypad
-	global lockMotor
-	
-	disp.writeMessage("Unlocked")
-	indicator.showGreen()
-	state = UNLOCKED
-	keypad.reset()
-	lockMotor.unlock()
+    global disp
+    global indicator
+    global state
+    global keypad
+    global lockMotor
+    
+    disp.writeMessage("Unlocked")
+    indicator.showGreen()
+    state = UNLOCKED
+    keypad.reset()
+    lockMotor.unlock()
 
 def displayCode():
     """ Display the code that's being typed on the LCD """
-	disp.displayCode(keypad.getGuess())
-	
+    disp.displayCode(keypad.getGuess())
+    
 def checkGuess():
     """ if the lock is in the LOCKED state, check if the guess is correct. Else set the passcode to be the guess. """
-	global state
-	global keypad
-	global passcode
-	global indicator
-	global lock
-	
-	if state == LOCKED:
-		guess = keypad.getGuess()
-		for i in range(len(guess)):
-			if int(guess[i]) != int(passcode[i]):
-				disp.writeMessage("Still Locked")
-				keypad.reset()
-				guess = []
-				break
-				
-		if guess != []:
-			unlock()
-			
-	else:
-		lock()
-	
+    global state
+    global keypad
+    global passcode
+    global indicator
+    global lock
+    
+    if state == LOCKED:
+        guess = keypad.getGuess()
+        for i in range(len(guess)):
+            if int(guess[i]) != int(passcode[i]):
+                disp.writeMessage("Still Locked")
+                keypad.reset()
+                guess = []
+                break
+                
+        if guess != []:
+            unlock()
+            
+    else:
+        lock()
+    
 ##############################
 #   Connect event handlers   #
 ##############################
@@ -108,15 +108,15 @@ while True:
     # I don't think this really does anything useful anymore, but 
     # I'm not testing anymore, so I'll leave it in for the demo.
 
-	if state == LOCKED and previousState == UNLOCKED:
-		guess = keypad.getGuess()
-		if len(guess) == 0:
-			disp.writeMessage("Locked")
-			
-	elif state == UNLOCKED and previousState == LOCKED:
-		guess = keypad.getGuess()
-		if len(guess) == 0:
-			disp.writeMessage("Unlocked")
-			
-	previousState = state
+    if state == LOCKED and previousState == UNLOCKED:
+        guess = keypad.getGuess()
+        if len(guess) == 0:
+            disp.writeMessage("Locked")
+            
+    elif state == UNLOCKED and previousState == LOCKED:
+        guess = keypad.getGuess()
+        if len(guess) == 0:
+            disp.writeMessage("Unlocked")
+            
+    previousState = state
 
